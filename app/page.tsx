2a1e0d3782e6fca262dc886c33be6486f284bda5
Dusit Pakdeekunakorn <S6552410011@sau.ac.th>
2025-09-04 13:40:46 +0700

    V0.1 Test2

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="">
-     food tracker app v0.1.0
+     food tracker app v0.1.0 Dusit65
     </div>
   );
 }
